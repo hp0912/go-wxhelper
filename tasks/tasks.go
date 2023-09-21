40e4dc48c6dc84ca99195ed3cda3fb04f32cceaa
李寻欢 <lxh@cxh.cn>
2023-09-21 17:36:31 +0800

    :art: 优化定时任务

--- tasks/tasks.go
+++ tasks/tasks.go
@@ -16,7 +16,7 @@ func InitTasks() {
 	_, _ = s.Every(1).Day().At("09:30").Do(yesterday)
 
 	// 每小时更新一次好友列表
-	_, _ = s.Every(1).Minute().Do(syncFriends)
+	_, _ = s.Every(1).Hour().Do(syncFriends)
 
 	// 开启定时任务
 	s.StartAsync()

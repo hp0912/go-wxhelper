4a348acfd8e9f650158c06cb91c0bb53080f9070
李寻欢 <lxh@cxh.cn>
2023-09-26 15:03:04 +0800

    :bug: Fix a bug.

--- tasks/water_group.go
+++ tasks/water_group.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"go-wechat/client"
 	"go-wechat/entity"
+	"go-wechat/utils"
 	"log"
 	"os"
 	"strings"
@@ -73,5 +74,5 @@ func dealYesterday(gid string) {
 	notifyMsgs = append(notifyMsgs, " \n请未上榜的群友多多反思。")
 
 	log.Printf("排行榜: \n%s", strings.Join(notifyMsgs, "\n"))
-	//go utils.SendMessage(gid, "", strings.Join(notifyMsgs, "\n"))
+	go utils.SendMessage(gid, "", strings.Join(notifyMsgs, "\n"))
 }

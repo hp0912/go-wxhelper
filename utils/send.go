6626f45af2d16c08070c0e49e16d57969c67d041
李寻欢 <lxh@cxh.cn>
2023-11-29 10:26:47 +0800

    :fire: 移除测试代码

--- utils/send.go
+++ utils/send.go
@@ -14,9 +14,6 @@ import (
 // @param atId
 // @param msg
 func SendMessage(toId, atId, msg string, retryCount int) {
-	if toId != "" {
-		return
-	}
 	if retryCount > 5 {
 		log.Printf("重试五次失败，停止发送")
 		return
@@ -60,10 +57,6 @@ func SendMessage(toId, atId, msg string, retryCount int) {
 // @param imgPath string 图片路径
 // @param retryCount int 重试次数
 func SendImage(toId, imgPath string, retryCount int) {
-	if toId != "" {
-		return
-	}
-
 	if retryCount > 5 {
 		log.Printf("重试五次失败，停止发送")
 		return

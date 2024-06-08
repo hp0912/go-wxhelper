9fdb0a6c9030a78e1913d360de15319b535a7fd3
hp0912 <809211365@qq.com>
2024-06-08 17:57:38 +0800

    feat: 词云调试日志

--- plugin/plugins/command/wordcloud.go
+++ plugin/plugins/command/wordcloud.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"go-wechat/config"
 	"go-wechat/utils"
+	"log"
 	"time"
 )
 
@@ -13,11 +14,14 @@ import (
 func WordCloud(userId string) {
 	res, ok := config.Conf.Resource["wordcloud"]
 	if !ok {
+		log.Printf("获取词云路径失败~")
 		return
 	}
 	// 获取昨日日期
 	yd := time.Now().Local().AddDate(0, 0, -1).Format("20060102")
-	// 发送词云
 	fileName := fmt.Sprintf("%s_%s.png", yd, userId)
+
+	// 发送词云
+	log.Printf("群ID: %s  词云路径: %s~", userId, fmt.Sprintf(res.Path, fileName))
 	utils.SendImage(userId, fmt.Sprintf(res.Path, fileName), 0)
 }
